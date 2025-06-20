c0d9f82df7a553b6cf83e6b31a5fa780caa5a3de
Zuhair binFarhan <zuhairfarhan@gmail.com>
2025-06-20 23:32:34 +0500

    added custom template.

--- Lapp.py
+++ Lapp.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/', methods = ['GET'])
 
-def hello():
-    return 'As-Salaamu Alaikum! This is my first Flask app!'
+# def hello():
+#     return 'As-Salaamu Alaikum! This is my first Flask app!'
+
+def render_home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(port = 2100, debug = True)
